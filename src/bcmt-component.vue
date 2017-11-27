f91d9b430cd1e523b4fc091e1dd407f4e8b2e28d
elisabethP <elisabeth.pointal@free.fr>
2017-11-27 21:54:01 +0100

    test slot without name

--- src/bcmt-component.vue
+++ src/bcmt-component.vue
@@ -2,13 +2,29 @@
 <span>
 <div>truc muche</div>
 <formater-layout >
-		<span slot="header"><h1>BCMT</h1></span>
+		<header>
+	<div class="formater-header">
+	</div>
+	</header>
 	
-		    <bcmt-form slot="sidebar"></bcmt-form>
-
-			<bcmt-map slot="content"></bcmt-map>
-		
-		<span slot="footer">pied de page</span>
+	<div class="formater-wrapper">
+		<aside>
+		<div class="formater-sidebar">
+			<bcmt-form></bcmt-form>
+			
+		</div>
+		</aside>
+		<main>
+		<div class="formater-content">
+			<bcmt-map></bcmt-map>
+		</div>
+		</main>
+	</div>
+	<footer>
+	<div class="formater-footer">
+	</div>
+	</footer>
 	</formater-layout>
+	
 	</span>
 </template>
